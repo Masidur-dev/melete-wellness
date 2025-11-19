d83ca21ad8d57c9f7ccaaeb0b8bbc6be22a91cec
Masidur-dev <hello.masidur.dev@gmail.com>
2025-11-20 03:05:51 +0530

    js

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const logoEl = document.getElementById("logo")
 
 const homeEl = document.getElementById("home")
 const aboutEl = document.getElementById("About")
+const expertEl = document.getElementById("Experts")
+const severiceEl = document.getElementById("Service")
