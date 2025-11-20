629da80118584251316997f79c0554d57a364679
Masidur-dev <hello.masidur.dev@gmail.com>
2025-11-21 00:10:07 +0530

    js

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const homeEl = document.getElementById("home")
 const aboutEl = document.getElementById("About")
 const expertEl = document.getElementById("Experts")
 const severiceEl = document.getElementById("Service")
+
+
+const heroSectionEl = document.querySelector(".hero-section")
+const aboutSectionEl = document.querySelector(".about-section")
