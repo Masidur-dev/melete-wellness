7dc9cb653c02d632b9e94919107ba45d80534f57
Masidur-dev <hello.masidur.dev@gmail.com>
2025-11-22 15:33:35 +0530

    js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const logoEl = document.getElementById("logo")
 const homeEl = document.getElementById("home")
 const aboutEl = document.getElementById("About")
 const expertEl = document.getElementById("Experts")
-const severiceEl = document.getElementById("Service")
+const severiceEl = document.getElementById("Service") 
+const priceEl = document.getElementById("price") 
 
 
 const heroSectionEl = document.querySelector(".hero-section")
 const aboutSectionEl = document.querySelector(".about-section")
+const hero2SectionnEl = document.querySelector(".hero2-section")
+const improveSectionEl = document.querySelector(".improve-section")
+const pricingLightSectionEl = document.querySelector(".pricing-light-section")
